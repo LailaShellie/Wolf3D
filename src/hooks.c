ee0ab6bc5005cb41e0f2e0dde7edfc06255ac7a0
root <ssoraka@student.21-school.ru>
2021-02-17 13:18:43 +0300

    add stolknoveniya

--- src/hooks.c
+++ src/hooks.c
@@ -12,21 +12,45 @@
 
 #include "../includes/wolf3d.h"
 
+#define TRUE 1
+#define FALSE 0
+
+#define STEP 0.1
+#define FORWARD 1
+#define BACKWARD -1
+
+//дублируется в файле map.c
+#define EMPTY 0
+
+int			ft_close(t_wolf3d *wlf)
+{
+	(void)wlf;
+	exit(0);
+}
+
+int			set_new_pos(t_vect2d *old, t_vect2d *dir, int move, t_map *map)
+{
+	t_vect2d new_p;
+
+	new_p.x = old->x + dir->x * STEP * move;
+	new_p.y = old->y + dir->y * STEP * move;
+	if (new_p.x >= 0 && new_p.x < map->width
+	&& new_p.y >= 0 && new_p.y < map->heigth
+	&& map->map[(int) new_p.y][(int) new_p.x] == EMPTY)
+	{
+		*old = new_p;
+		return (TRUE);
+	}
+	return (FALSE);
+}
+
 int 		keyhooks(int key, t_wolf3d *wlf)
 {
 	printf("%d - key\n", key);
-	if (key == KEY_UP)
-	{
-		wlf->pos.x += wlf->dir.x * 0.1;
-		wlf->pos.y += wlf->dir.y * 0.1;
+	if (key == KEY_UP && set_new_pos(&wlf->pos, &wlf->dir, FORWARD, &wlf->map))
 		render(wlf);
-	}
-	if (key == KEY_DOWN)
-	{
-		wlf->pos.x -= wlf->dir.x * 0.1;
-		wlf->pos.y -= wlf->dir.y * 0.1;
+	if (key == KEY_DOWN && set_new_pos(&wlf->pos, &wlf->dir, BACKWARD, &wlf->map))
 		render(wlf);
-	}
 	if (key == KEY_LEFT)
 	{
 		wlf->dir = rot_vect2d(&wlf->dir, -0.1);
@@ -40,14 +64,8 @@ int 		keyhooks(int key, t_wolf3d *wlf)
 		render(wlf);
 	}
 	if (key == KEY_ESC)
-		exit(0);
+		ft_close(wlf);
 	return (1);
-}
-
-int			ft_close(t_wolf3d *wlf)
-{
-	(void)wlf;
-	exit(0);
 }
 
 void		hooks(t_wolf3d *wlf)
