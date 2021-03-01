2210c8e05eb283bddb0f50e31da46c3a04ccfb94
root <ssoraka@student.21-school.ru>
2021-03-01 23:48:39 +0300

    под

--- src/render.c
+++ src/render.c
@@ -69,47 +69,91 @@ void	draw_line(t_wolf3d *wlf, t_ray *ray)
 	manage_texture(wlf, ray);
 }
 
-void	cast_ray(t_wolf3d *wlf, t_ray *ray)
-{
-	double	side_x;
-	double	side_y;
 
-	side_x = ray->dir.x >= 0 ? (1.0 - wlf->pos.x + ray->map_x)
-			* ray->delta_x : (wlf->pos.x - ray->map_x) * ray->delta_x;
-	side_y = ray->dir.y >= 0 ? (1.0 - wlf->pos.y + ray->map_y)
-			* ray->delta_y : (wlf->pos.y - ray->map_y) * ray->delta_y;
-	ray->step_x = ray->dir.x >= 0 ? 1 : -1;
-	ray->step_y = ray->dir.y >= 0 ? 1 : -1;
-	while (ray->hit == 0 && ray->map_x > 0 && ray->map_x < wlf->map.width - 1 && ray->map_y > 0 && ray->map_y < wlf->map.heigth - 1)
+void	get_hit(t_wolf3d *wlf, t_ray *ray, t_vect2d side)
+{
+	while (ray->hit == 0 && ray->map_x > 0 && ray->map_x < wlf->map.width - 1
+	&& ray->map_y > 0 && ray->map_y < wlf->map.heigth - 1)
 	{
-		if (side_x < side_y)
+		if (side.x < side.y)
 		{
-			side_x += ray->delta_x;
+			side.x += ray->delta_x;
 			ray->side = X_SIDE;
 			ray->map_x += ray->step_x;
 		}
 		else
 		{
-			side_y += ray->delta_y;
+			side.y += ray->delta_y;
 			ray->side = Y_SIDE;
 			ray->map_y += ray->step_y;
 		}
 		ray->hit = wlf->map.map[ray->map_y][ray->map_x];
 	}
-	if (ray->hit == 0)
+}
+
+void	cast_ray(t_wolf3d *wlf, t_ray *ray)
+{
+	t_vect2d	side;
+
+	side.x = ray->dir.x >= 0 ? (1.0 - wlf->pos.x + ray->map_x)
+			* ray->delta_x : (wlf->pos.x - ray->map_x) * ray->delta_x;
+	side.y = ray->dir.y >= 0 ? (1.0 - wlf->pos.y + ray->map_y)
+			* ray->delta_y : (wlf->pos.y - ray->map_y) * ray->delta_y;
+	ray->step_x = ray->dir.x >= 0 ? 1 : -1;
+	ray->step_y = ray->dir.y >= 0 ? 1 : -1;
+	get_hit(wlf, ray, side);
+	if (ray->hit <= 0)
 		return ;
-	ray->hit = (ray->hit - 1) % (TEX_NUM); //
+	ray->hit = (ray->hit - 1) % (TEX_NUM);
+	//функцию поправить и норм.
+	if (ray->step_x == 1 && ray->side == X_SIDE)
+		ray->hit = 0;
+	if (ray->step_x == -1 && ray->side == X_SIDE)
+		ray->hit = 1;
+	if (ray->step_y == 1 && ray->side == Y_SIDE)
+		ray->hit = 2;
+	if (ray->step_y == -1 && ray->side == Y_SIDE)
+		ray->hit = 3;
 	draw_line(wlf, ray);
+}
+
+void	draw_floor(t_wolf3d *wlf, int y, t_vect2d floor, t_vect2d floor_step)
+{
+	int x;
+	int tx;
+	int ty;
+	int cell_x;
+	int cell_y;
+
+	x = -1;
+	while (++x < W)
+	{
+		cell_x = (int)(floor.x);
+		cell_y = (int)(floor.y);
+		tx = (int)(wlf->text[4].width * (floor.x - cell_x))
+				& (wlf->text[4].width - 1);
+		ty = (int)(wlf->text[4].height * (floor.y - cell_y))
+				& (wlf->text[4].height - 1);
+		wlf->img->data[y * W + x] =
+				wlf->text[4].buf[wlf->text[4].width * ty + tx];
+		wlf->img->data[(H - y - 1) * W + x] =
+				wlf->text[2].buf[wlf->text[2].width * ty + tx];
+		floor.x += floor_step.x;
+		floor.y += floor_step.y;
+	}
 }
 
 void	render_floor(t_wolf3d *wlf)
 {
-	t_vect2d floor;
-	t_vect2d floor_step;
-	t_vect2d ray_dir0;
-	float row_dist;
+	t_vect2d	floor;
+	t_vect2d	floor_step;
+	t_vect2d	ray_dir0;
+	float		row_dist;
+	int			y;
 
-	for(int y = 0; y < H / 2 ; y++) {
+	y = -1;
+	while (++y < H / 2)
+	{
 		ray_dir0.x = wlf->dir.x + wlf->dir.y;
 		ray_dir0.y = wlf->dir.y - wlf->dir.x;
 		row_dist = (0.5 * H) / (H / 2 - y);
@@ -117,43 +161,18 @@ void	render_floor(t_wolf3d *wlf)
 		floor_step.y = row_dist * (wlf->dir.y + wlf->dir.x - ray_dir0.y) / W;
 		floor.x = wlf->pos.x + row_dist * ray_dir0.x;
 		floor.y = wlf->pos.y + row_dist * ray_dir0.y;
-
-
-
-		int x;
-		int tx;
-		int ty;
-		int cell_x;
-		int cell_y;
-
-		x = -1;
-		while (++x < W)
-		{
-			cell_x = (int)(floor.x);
-			cell_y = (int)(floor.y);
-
-			tx = (int)(wlf->text[4].width * (floor.x - cell_x)) & (wlf->text[4].width - 1);
-			ty = (int)(wlf->text[4].height * (floor.y - cell_y)) & (wlf->text[4].height - 1);
-			wlf->img->data[y * W + x] = wlf->text[4].buf[wlf->text[4].width * ty + tx];
-
-			tx = (int)(wlf->text[2].width * (floor.x - cell_x)) & (wlf->text[2].width - 1);
-			ty = (int)(wlf->text[2].height * (floor.y - cell_y)) & (wlf->text[2].height - 1);
-			wlf->img->data[(H - y - 1) * W + x] = wlf->text[2].buf[wlf->text[2].width * ty + tx];
-			floor.x += floor_step.x;
-			floor.y += floor_step.y;
-		}
+		draw_floor(wlf, y, floor, floor_step);
 	}
 }
-
 
 void	render(t_wolf3d *wlf)
 {
 	t_ray	ray;
-	double 	delta;
+	double	delta;
 
-	wlf->x = -1;
 	ft_bzero(wlf->img->data, W * H * 4);
 	render_floor(wlf);
+	wlf->x = -1;
 	while (++wlf->x < W)
 	{
 		ft_bzero(&ray, sizeof(t_ray));
@@ -164,7 +183,8 @@ void	render(t_wolf3d *wlf)
 		ray.dir.y = wlf->dir.y + wlf->dir.x * delta;
 		ray.delta_x = fabs(1 / ray.dir.x);
 		ray.delta_y = fabs(1 / ray.dir.y);
-		cast_ray(wlf , &ray);
+		cast_ray(wlf, &ray);
 	}
-	mlx_put_image_to_window(wlf->mlx_ptr, wlf->win_ptr, wlf->img->img_ptr, 0, 0);
+	mlx_put_image_to_window(
+			wlf->mlx_ptr, wlf->win_ptr, wlf->img->img_ptr, 0, 0);
 }
